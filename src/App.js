33e5b86c365c1a5950bd45a18e79f03ba4c067a7
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-01-30 13:27:49 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Hello = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello world {props.name}, you are {props.age}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  )
+}
+
+export default App
