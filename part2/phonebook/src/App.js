1fbe67da68c6cee50908c5785279d42fdce8ead8
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-02-14 23:25:59 +0000

    part 2 up to exercise 2.16 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Content from './components/Content'
 import Filter from './components/Filter'
 import Form from './components/Form'
+import personService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+    }, [])
 
   const handleChange = (setValue) => (event) => {
     console.log(event.target.value)
@@ -24,21 +27,65 @@ const App = () => {
     event.preventDefault()
     console.log("button clicked", event.target)
 
-    if (persons.find(person => person.name === newName)) {
-      return alert(`${newName} is already added to phonebook`)
-    }
+    if (newName === "" || newNumber === "") {
+      return alert("No name or number")
+    } 
 
     const nameObject = {
       name: newName,
       number: newNumber
     }
-  
-    setPersons(persons.concat(nameObject))
-    setNewName('')
-    setNewNumber('')
+
+    if (persons.find(person => person.name === newName)) {
+      const personToUpdate = persons.find(person => person.name === newName)
+      console.log(personToUpdate)
+      const tempPersons = persons.filter(person => person.name !== personToUpdate.name)
+
+      personService
+      .update(personToUpdate.id, nameObject)
+      .then(returnedPerson => {
+        const updatedPersons = tempPersons.concat(returnedPerson)
+        console.log(updatedPersons)
+        setPersons(updatedPersons)
+        setNewName('')
+        setNewNumber('')
+        console.log(returnedPerson)
+      })
+
+      console.log(persons)
+    }
+    
+    else {
+      personService
+      .create(nameObject)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+        console.log(returnedPerson)
+      })
+
     console.log(persons)
+    }
   }
 
+  const deleteNameNumber = (id) => (event) => {
+    console.log("button pressed", event.target)
+    console.log(`${id} to be deleted`)
+
+    const delPerson = persons.find(person => person.id === id)
+
+    if (window.confirm(`Delete ${delPerson.name}?`)) {
+      personService
+      .del(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id ))
+      })
+    }
+
+    console.log(persons)
+  }
+  
   return (
     <div>
       <h2>Phonebook</h2>
@@ -47,7 +94,7 @@ const App = () => {
       <h2>Add name and number</h2>
       <Form onSubmit={addNameNumber} value1={newName} value2={newNumber} handleChange={handleChange} set1={setNewName} set2={setNewNumber}/>
       <h2>Numbers</h2>
-      <Content persons={persons} filter={newFilter}/>
+      <Content persons={persons} newFilter={newFilter} onClick={deleteNameNumber}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Content.js
+++ part2/phonebook/src/components/Content.js
@@ -1,3 +1,10 @@
-const Content = ( {persons, filter} ) => persons.filter(person => person.name.toLowerCase().includes(filter)).map(person => <p>{person.name} {person.number}</p>)
+const Content = ( {persons, newFilter, onClick} ) => {
+    const contentToShow = persons
+        .filter(person => person.name.toLowerCase().includes(newFilter))
+        .map(person => <p>{person.name} {person.number} <button id={person.id+"button"} onClick={onClick(person.id)}>Delete</button></p>)
+
+    return (contentToShow)
+}
+
 
 export default Content
