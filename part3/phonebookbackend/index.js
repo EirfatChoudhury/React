6c9335a127ee27cb6379ff5f3be7cc26c28e3734
Eirfat Choudhury <eirfathchoudhury@gmail.com>
2023-02-15 16:48:57 +0000

    render deploy test

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,9 +1,28 @@
 const express = require("express")
 const morgan = require("morgan")
-app = express()
-app.use(morgan('tiny'))
+const cors = require('cors')
 
-const PORT = 3001
+app = express()
+app.use(express.json())
+app.use(express.static('build'))
+
+morgan.token('person', function (req, res) { return JSON.stringify(req.body) })
+app.use(
+    morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms',
+      tokens.person(req, res)
+    ].join(' ')
+  })
+)
+
+app.use(cors())
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
 
